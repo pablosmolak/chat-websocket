adee508962fffd0c649084fe5aeb5d4ff5134311
Pablo Smolak <pablosmolak2004@hotmail.com>
2024-12-11 23:44:44 -0400

    ajustando rots

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@ import cors from 'cors';
 import db from "./config/db_config.js";
 import setupWebSocket from './websockets/websocket.js';
 
+//para teste
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // App setup
 const app = express();
 const server = http.createServer(app);
@@ -15,6 +21,19 @@ app.use(express.urlencoded({ extended: false }));
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
     console.log("Conectado ao banco de dados.");
+});
+
+// Servir arquivos estáticos (se necessário, como CSS ou imagens no futuro)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota para retornar o front.html
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/pages', express.static(path.join(__dirname, 'src', 'pages')));
+
+// Rota para retornar o front.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages', 'front.html'));
 });
 
 setupWebSocket(server);
